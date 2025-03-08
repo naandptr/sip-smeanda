f15368b699e0ea24396d1811d2180e360720969a
naandptr <nandaaputr07@gmail.com>
2025-03-09 03:09:10 +0700

    update view modal detail jurnal

--- resources/views/components/modal_detail_jurnal.blade.php
+++ resources/views/components/modal_detail_jurnal.blade.php
@@ -1,22 +1,22 @@
 <div class="modal fade" id="modalDetailJurnal" tabindex="-1" role="dialog" aria-labelledby="modalDetailJurnal" aria-hidden="true">
     <div class="modal-dialog custom-modal" role="document">
-        <div class="modal-content modal-siswa-content">
-            <div class="modal-siswa-body view-container">
-                <div class="view-item">
+        <div class="modal-content">
+            <div class="modal-view-body">
+                <div class="modal-view-item">
                     <h5>Nama Lengkap</h5>
-                    <div class="view-value">
+                    <div class="modal-view-value">
                         <h5>Arslan Allen</h5>
                     </div>
                 </div>
-                <div class="view-item">
+                <div class="modal-view-item">
                     <h5>Tanggal</h5>
-                    <div class="view-value">
+                    <div class="modal-view-value">
                         <h5>20/01/2025</h5>
                     </div>
                 </div>
-                <div class="view-item">
+                <div class="modal-view-item">
                     <h5>Deskripsi Kegiatan</h5>
-                    <div class="view-value desc-box">
+                    <div class="modal-view-value desc-box">
                         <h5>Kegiatan yang dilaksanakan hari ini adalah...</h5>
                     </div>
                 </div>
