ab8ddaeef5aab5462c3fc837f9bf3bb73ad8c56a
naandptr <nandaaputr07@gmail.com>
2025-03-09 02:57:20 +0700

    update view modal validasi

--- resources/views/components/modal_jurnal.blade.php
+++ resources/views/components/modal_jurnal.blade.php
@@ -3,14 +3,12 @@
         <div class="modal-content">
             <form action="/jurnal" method="POST" id="formJurnal">
                 <div class="modal-form-body">
-                    <!-- Input Tanggal -->
                     <div class="modal-form-group">
                         <label for="tglJurnal">Tanggal<span class="required-label">*</span></label>
                         <div class="modal-form-value">
                             <input type="date" name="tglJurnal" id="tglJurnal" required>
                         </div>
                     </div>
-                    <!-- Input Deskripsi -->
                     <div class="modal-form-group">
                         <label for="summernote">Deskripsi<span class="required-label">*</span></label>
                         <div class="modal-form-value">
@@ -19,7 +17,6 @@
                     </div>
 
                 </div>
-                <!-- Footer Modal -->
                 <div class="modal-form-footer">
                     <button type="button" class="btn-cancel" data-bs-dismiss="modal">Cancel</button>
                     <button type="submit" class="btn-submit">Submit</button>
--- resources/views/components/modal_validasi.blade.php
+++ resources/views/components/modal_validasi.blade.php
@@ -1,16 +1,16 @@
 <div class="modal fade" id="modalValidasiJurnal" tabindex="-1" role="dialog" aria-labelledby="modalValidasiJurnal" aria-hidden="true">
     <div class="modal-dialog custom-modal" role="document">
-        <div class="modal-content modal-validasi-content">
-            <form action="/validasi" method="POST" class="validasi-form" id="formValidasi">
-                <div class="modal-validasi-body validasi-container">
-                    <div class="validasi-header">
-                        <h4>Masukkan Alasan</h4>
-                    </div>
-                    <div class="validasi-value">
+        <div class="modal-content">
+            <form action="/validasi" method="POST" id="formValidasi">
+                <div class="modal-form-header">
+                    <h4>Masukkan Alasan</h4>
+                </div>
+                <div class="modal-form-body">
+                    <div class="modal-form-value validasi-value">
                         <textarea name="komentar" id="komentar" cols="30" rows="10" placeholder="Jelaskan alasan..."></textarea>
                     </div>
-                    <div class="modal-validasi-footer">
-                        <button type="button" class="close btn-cancel" data-bs-dismiss="modal" aria-label="Close">Cancel</button>
+                    <div class="modal-form-footer">
+                        <button type="button" class="btn-cancel" data-bs-dismiss="modal" aria-label="Close">Cancel</button>
                         <button type="submit" class="btn-submit">Submit</button>
                     </div>
                 </div>
