17f4fa0f193caa32d29294e6f23d0373fb8584f7
naandptr <nandaaputr07@gmail.com>
2025-03-09 02:47:54 +0700

    update view modal absen dan jurnal

--- resources/views/components/modal_absen.blade.php
+++ resources/views/components/modal_absen.blade.php
@@ -1,26 +1,26 @@
-<div class="modal fade" id="modalAbsen" tabindex="-1" role="dialog" aria-labelledby="modalTambahAbsen" aria-hidden="true">
+<div class="modal fade" id="modalAbsen" tabindex="-1" role="dialog" aria-labelledby="modalAbsen" aria-hidden="true">
     <div class="modal-dialog" role="document">
-        <div class="modal-content modal-absen-content">
-            <form action="/absen" method="POST" class="absen-form" id="formAbsen">
-                <div class="modal-absen-body">
-                    <div class="input-item">
+        <div class="modal-content">
+            <form action="/absen" method="POST" id="formAbsen">
+                <div class="modal-form-body">
+                    <div class="modal-form-group">
                         <label for="tglAbsenFormatted">Tanggal</label>
-                        <div class="input-value">
+                        <div class="modal-form-value">
                             <input type="text" name="tglAbsenFormatted" id="tglAbsenFormatted" readonly required>
                         </div>
                     </div>
-                    <div class="input-item">
-                        <label for="jenisAbsen">Jenis<span style="color: red;">*</span></label>
-                        <div class="input-value">
+                    <div class="modal-form-group">
+                        <label for="jenisAbsen">Jenis<span class="required-label">*</span></label>
+                        <div class="modal-form-value">
                             <select id="jenisAbsen" name="jenisAbsen" required>
                                 <option value="absen-datang">Absen Datang</option>
                                 <option value="absen-pulang">Absen Pulang</option>
                             </select>
                         </div>
                     </div>
-                    <div class="input-item">
-                        <label for="statusAbsen">Status<span style="color: red;">*</span></label>
-                        <div class="input-value">
+                    <div class="modal-form-group">
+                        <label for="statusAbsen">Status<span class="required-label">*</span></label>
+                        <div class="modal-form-value">
                             <select id="statusAbsen" name="statusAbsen" required>
                                 <option value="status-hadir">Hadir</option>
                                 <option value="status-izin">Izin</option>
@@ -28,23 +28,23 @@
                             </select>
                         </div>
                     </div>
-                    <div class="input-item">
-                        <label for="ketAbsen">Keterangan<span style="color: red;">*</span></label>
-                        <div class="input-value">
+                    <div class="modal-form-group">
+                        <label for="ketAbsen">Keterangan<span class="required-label">*</span></label>
+                        <div class="modal-form-value">
                             <input type="text" id="ketAbsen" name="ketAbsen">
                         </div>
                         
                     </div>
-                    <div class="input-item">
-                        <label for="fileAbsen">Upload Foto<span style="color: red;">*</span></h5></label>
-                        <div class="input-value">
+                    <div class="modal-form-group">
+                        <label for="fileAbsen">Upload Foto<span class="required-label">*</span></h5></label>
+                        <div class="modal-form-value">
                             <input type="file" class="filepond" name="fileAbsen[]" id="fileAbsen">
                         </div>
                     </div>
                 </div>
 
-                <div class="modal-absen-footer">
-                    <button type="button" class="close btn-cancel" data-bs-dismiss="modal" aria-label="Close">Cancel</button>
+                <div class="modal-form-footer">
+                    <button type="button" class="btn-cancel" data-bs-dismiss="modal">Cancel</button>
                     <button type="submit" class="btn-submit">Submit</button>
                 </div>
             </form>
--- resources/views/components/modal_jurnal.blade.php
+++ resources/views/components/modal_jurnal.blade.php
@@ -1,23 +1,27 @@
-<div class="modal fade" id="modalJurnal" tabindex="-1" role="dialog" aria-labelledby="modalTambahJurnal" aria-hidden="true">
-    <div class="modal-dialog custom-modal" role="document">
-        <div class="modal-content modal-jurnal-content">
-            <form action="/jurnal" method="POST" class="jurnal-form" id="formJurnal">
-                <div class="modal-jurnal-body">
-                    <div class="input-item">
-                        <label for="tglJurnal">Tanggal<span style="color: red;">*</span></label>
-                        <div class="input-value">
+<div class="modal fade" id="modalJurnal" tabindex="-1" aria-labelledby="modalJurnal" aria-hidden="true">
+    <div class="modal-dialog custom-modal">
+        <div class="modal-content">
+            <form action="/jurnal" method="POST" id="formJurnal">
+                <div class="modal-form-body">
+                    <!-- Input Tanggal -->
+                    <div class="modal-form-group">
+                        <label for="tglJurnal">Tanggal<span class="required-label">*</span></label>
+                        <div class="modal-form-value">
                             <input type="date" name="tglJurnal" id="tglJurnal" required>
                         </div>
                     </div>
-                    <div class="input-item">
-                        <label for="summernote">Deskripsi<span style="color: red;">*</span></label>
-                        <div class="input-value">
-                            <textarea id="summernote" name="content"></textarea>
+                    <!-- Input Deskripsi -->
+                    <div class="modal-form-group">
+                        <label for="summernote">Deskripsi<span class="required-label">*</span></label>
+                        <div class="modal-form-value">
+                            <textarea id="summernote" name="content" required></textarea>
                         </div>
                     </div>
+
                 </div>
-                <div class="modal-jurnal-footer" id="modalFooter">
-                    <button type="button" class="close btn-cancel" data-bs-dismiss="modal" aria-label="Close">Cancel</button>
+                <!-- Footer Modal -->
+                <div class="modal-form-footer">
+                    <button type="button" class="btn-cancel" data-bs-dismiss="modal">Cancel</button>
                     <button type="submit" class="btn-submit">Submit</button>
                 </div>
             </form>
